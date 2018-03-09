b01d6d1bb1f19df053b4a6b9824573c8b6d44017
Neeraj Mula <neeraj.mula@gmail.com>
2018-03-08 22:18:11 -0500

    Auto
    Created a better auto organization. The paths are not complete tho.

---
+++ src/org/usfirst/frc/team2554/robot/commands/auto/CenterLineCross.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team2554.robot.commands.auto;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class CenterLineCross extends CommandGroup {
+
+    public CenterLineCross() {
+        
+    }
+}
---
+++ src/org/usfirst/frc/team2554/robot/commands/auto/SideLineCross.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team2554.robot.commands.auto;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class SideLineCross extends CommandGroup {
+
+    public SideLineCross() {
+       
+    }
+}
